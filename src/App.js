16d83b5dcb95f677bcd1385ebca47cef163e25ab
codeKingMike <codekingmike@gmail.com>
2021-03-30 13:46:20 -0700

    Doing a tutorial

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import data from './data';
+import SingleQuestion from './Question';
+
+function App() {
+  return <h2>accordion app</h2>;
+}
+
+export default App;
---
+++ src/Question.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-items/ai';
+
+const Question = () => {
+    return <h2>questions component</h2>
+}
+
+export default Question;
